9563d7b59108a1ca76a1c9d347e7e28b2118fffa
e143858 <zachary.herrera@wnco.com>
2020-05-25 13:15:08 -0500

    combined technical indicators with aggregator and added a few custom technical alerts I use

--- aggregator.py
+++ aggregator.py
@@ -1,15 +1,18 @@
+import datetime
 import time
 
 import config as cfg
 import pandas as pd
+from alpha_vantage.techindicators import TechIndicators
 from alpha_vantage.timeseries import TimeSeries
 
 api_key = cfg.Secrets["api_key"]
 
 ts = TimeSeries(key=api_key, output_format="pandas")
+ti = TechIndicators(key=api_key, output_format="pandas")
 # Testing API
-data, meta_data = ts.get_intraday(symbol="OAS", interval="1min", outputsize="full")
-print(data)
+# data, meta_data = ts.get_intraday(symbol="OAS", interval="1min", outputsize="full")
+# print(data)
 
 
 def get_intraday_data_one_minute(ticker):
@@ -128,5 +131,98 @@ def get_crypto_daily(ticker, market="USD"):
     Returns:
         pandas -- dataframe of daily data refreshed at midnight (UTC).
     """
-    data = ts.get_digital_currency_daily(ticker=ticker, market=market)
+    data = ts.get_digital_currency_daily(symbol=ticker, market=market)
     return data
+
+
+def get_percentage_change(ticker):
+    """Retrieve the percentage change in 5 minute increments
+
+    Arguments:
+        ticker {string} -- unique series of letters assigned to a security for trading purposes
+
+    Returns:
+        Pandas -- Formatted dataframe of percent change over 5 minute increments
+    """
+    data = ts.get_intraday(symbol=ticker, interval="5min", outputsize="full")
+    close_data = data["4. close"]
+    percentage_change = close_data.pct_change()
+    return percentage_change
+
+
+def set_alert_volatility(ticker, increased_change=0.0008):
+    """Day trading alert can be set on watched tickers
+
+    Arguments:
+        ticker {string} -- unique series of letters assigned to a security for trading purposes
+
+    Keyword Arguments:
+        increased_change {float} -- incremental change you are seeking within the 1 minute increments (default: {0.0008})
+
+    Returns:
+        Alert -- returns alert stamped with date & time of incremental change conditional statement
+    """
+    now = datetime.now()
+    alert_message = (
+        now.strftime("%I:%M:%S %p")
+        + " ALERT "
+        + ticker
+        + " Has increased above "
+        + increased_change
+        + " in the last minute"
+    )
+    data = ts.get_intraday(symbol=ticker, interval="1min", outputsize="full")
+    close_data = data["4. close"]
+    percentage_change = close_data.pct_change()
+    last_change = percentage_change[-1]
+    if abs(last_change) > increased_change:
+        return alert_message
+
+
+def get_simple_moving_average_bi_weekly(ticker):
+    """arithmetic moving average calculated by adding recent prices and dividing that by the number of time periods
+
+    Arguments:
+        ticker {string} -- unique series of letters assigned to a security for trading purposes
+
+    Returns:
+        Pandas -- Formatted dataframe of SMA
+    """
+    data_ti = ti.get_sma(symbol=ticker, interval="daily", time_period=14, series_type="close")
+    return data_ti
+
+
+def get_exponential_moving_average_bi_weekly(ticker):
+    """Type of moving average that places a greater weight and significance on most recent data points 
+
+    Arguments:
+        ticker {string} -- unique series of letters assigned to a security for trading purposes
+
+    Returns:
+        Pandas -- Formatted dataframe of EMA
+    """
+    data_ti = ti.get_ema(symbol=ticker, interval="daily", time_period=14, series_type="close")
+    return data_ti
+
+
+def get_sma_to_close_indicator(ticker, period=60):
+    """Looking for closing price much lower than SMA (betting the stock price will correct itself)
+
+    Arguments:
+        ticker {string} -- unique series of letters assigned to a security for trading purposes
+
+    Keyword Arguments:
+        period {int} -- number of time periods used to divide for SMA (default: {60})
+
+    Returns:
+        pandas -- Formatted data of close price to SMA 
+    """
+    data_ti = ti.get_sma(symbol=ticker, interval="1min", time_period=period, series_type="close")
+    data_ts = ts.get_intraday(symbol=ticker, interval="1min", outputsize="full")
+    dataframe1 = data_ti
+    dataframe2 = data_ts["4. close"].iloc[
+        period - 1 : :
+    ]  # Remove first 60 entries to have same amount of values in both frames
+    dataframe2.index = dataframe1.index
+    total_dataframe = pd.concat([dataframe1, dataframe2], axis=1)
+    return total_dataframe
